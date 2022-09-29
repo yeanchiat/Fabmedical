3b4725a33f3a0d018869956d1be3a46483df4188
yeanchiat <yeanchiat.wong@gmail.com>
2022-09-29 13:31:53 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("d91600db-ae39-42b5-a865-9a4894fca76d");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
